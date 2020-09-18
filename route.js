2e7c0ecc2d3ac43378543c96c51cba8efa1b2049
JavierESM <javierestebanmn@gmail.com>
2020-09-17 21:01:40 -0300

    banner

---
+++ route.js
@@ -0,0 +1,22 @@
+const express = require ("express")
+const app = express()
+const path = require ("path")
+app.listen(3000, () => console.log("Server up"))
+app.get('/', (req, res) => {
+    let file = path.resolve('vistas/ml.html')
+    res.sendFile(file)
+})
+app.get(`*`, (req, res) => {
+    if (req.url.endsWith('.css')) {
+        let file = path.resolve('public' + req.url)
+        return res.sendFile(file)
+    }
+
+let images = ['jpg', 'jpeg', 'gif', 'png','svg']
+let ext = req.url.split('.')[1]
+
+if (images.includes(ext)) {
+    let file = path.resolve('public' + req.url)
+    return res.sendFile(file)
+}} 
+) 
