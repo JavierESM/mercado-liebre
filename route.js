466ccd2fed4cbcb9e27fc7ff37b5ccc0a0038703
JavierESM <javierestebanmn@gmail.com>
2020-10-04 18:22:59 -0300

    added routing for login

--- route.js
+++ route.js
@@ -10,6 +10,10 @@ app.get('/registro', (req, res) => {
     let file = path.resolve('vistas/registro.html')
     res.sendFile(file)
 })
+app.get('/ingreso', (req, res) => {
+    let file = path.resolve('vistas/ingreso.html')
+    res.sendFile(file)
+})
 app.get(`*`, (req, res) => {
     if (req.url.endsWith('.css')) {
         let file = path.resolve('public' + req.url)
